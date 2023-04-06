2d8ee10cd9e4ae7d60e6b71115b7b3f19114cac9
Pranjal Kamboj <pranjalkamboj007@gmail.com>
2023-04-06 19:04:10 +0530

    Arrow function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+//Arrow functions
+const productOfTwo = (a,b) => {
+    return a*b;
+}
+console.log(productOfTwo(5,7));
+
+let person = {
+    name: "Max",
+    age: "20",
+    greet: () => {
+        console.log("My name is " + this.name)  
+    }
+}
+
+person.greet(); //undefined
