0c49744b10e58241710a6218b7d063ff50299a0b
Pranjal Kamboj <pranjalkamboj007@gmail.com>
2023-04-06 22:36:31 +0530

    Read & Write file

--- server.js
+++ server.js
@@ -1,18 +1,58 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    const url = req.url;
-    if (url === '/home'){
-        res.write('<html><head><body><h1>Welcome Home</h1></body></head></html>')
-    } else if (url ==='/about'){
-        res.write('<html><head><body><h1>Welcome to About Us page</h1></body></head></html>')
-    } else if(url === '/node'){
-        res.write('<html><head><body><h1>Welcome to my Node Js project</h1></body></head></html>')
+  res.setHeader('Content-Type', 'text/html');
+
+  if (req.url === '/') {
+    // Read the message from the file if it exists
+    let message = '';
+    try {
+      message = fs.readFileSync('message.txt', 'utf8');
+    } catch (err) {
+      console.error(err);
     }
-   res.end();
-})
+
+    // Render the HTML form with the message if it exists
+    res.write(`
+      <html>
+        <head>
+          <title>Node.js Form Example</title>
+        </head>
+        <body>
+            <p>${message}</p>
+          <form action="/message" method="post">
+            <label for="message">Message:</label>
+            <input type="text" id="message" name="message">
+            <button type="submit">Submit</button>
+          </form>
+        </body>
+      </html>
+    `);
+    return res.end();
+  }
+
+  if (req.url === '/message' && req.method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+
+      // Write the message to the file
+      fs.writeFileSync('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+
+  res.end();
+});
 
 server.listen(3000, () => {
-    console.log("Server is listening on port 3000")
-})
+  console.log('Server is listening on port 3000');
+});
