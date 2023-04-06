3d4477615d2a59cff6398b98a08040662cb27563
Pranjal Kamboj <pranjalkamboj007@gmail.com>
2023-04-06 21:48:04 +0530

    http response

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end("Pranjal");
+    res.setHeader('Content-Type', 'text/html');
+    const url = req.url;
+    if (url === '/home'){
+        res.write('<html><head><body><h1>Welcome Home</h1></body></head></html>')
+    } else if (url ==='/about'){
+        res.write('<html><head><body><h1>Welcome to About Us page</h1></body></head></html>')
+    } else if(url === '/node'){
+        res.write('<html><head><body><h1>Welcome to my Node Js project</h1></body></head></html>')
+    }
+   res.end();
 })
 
 server.listen(3000, () => {
-    console.log("Server is listening on PORT 3000");
+    console.log("Server is listening on port 3000")
 })
