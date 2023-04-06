254c19d357e9a6c21f05cd702e9fc1e5b97e4ddd
Pranjal Kamboj <pranjalkamboj007@gmail.com>
2023-04-06 19:31:36 +0530

    Map function

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ let person = {
 }
 
 person.greet(); //undefined
+
+//Map function
+const fruits = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+const newFruits = fruits.map((fruit) => {
+    if(fruit === ' '){
+        return 'empty string'
+    } else {
+        return fruit
+    }
+});
+
+console.log(newFruits);
+
+
+
+
+const obj1 = {'key1': 1 , 'key2' : 2}
+
+const obj2 = { ...obj1, key1: 1000}
+
+
+
+console.log(obj1)
+
+console.log(obj2)
